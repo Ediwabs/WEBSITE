7c691509bc62c713a4ca0b91db19b8c43af90b70
Ediwabs <146497087+Ediwabs@users.noreply.github.com>
2024-07-27 01:00:33 -0500

    Add files via upload

---
+++ Charlottie/js/scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.getElementById("sidebar").classList.toggle('active');
+    document.getElementById("content").classList.toggle('active');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("JavaScript is working!");
+});
